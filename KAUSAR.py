b53b8e316f373a346329db58fb7be2843b8dcc9b
ᴍʀ.ᴋᴀᴜsᴀʀ‏ <ahkausar2@gmail.com>
2022-09-11 15:39:46 +0600

    Create KAUSAR.py

---
+++ KAUSAR.py
@@ -0,0 +1,24 @@
+import os, platform
+
+try:
+
+        import requests
+
+except:
+
+        os.system('pip2 install requests')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == "64bit":
+
+        from RANDOM_64 import Main
+
+        Main()
+
+if bit == "32bit":
+        from RENDOM_32 import Main
+
+        Main()
