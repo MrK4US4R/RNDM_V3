98896ef303f3a5dbb21ae4973423a795d63530b3
ᴍʀ.ᴋᴀᴜsᴀʀ‏ <ahkausar2@gmail.com>
2022-09-11 16:00:53 +0600

    Update KAUSAR.py

--- KAUSAR.py
+++ KAUSAR.py
@@ -14,9 +14,10 @@ bit = platform.architecture()[0]
 
 if bit == "64bit":
 
-        from RANDOM_64 import Main
-
+        from RANDOM_64 import Main;x
         Main()
+        x()
+
 
 if bit == "32bit":
         from RENDOM_32 import Main
